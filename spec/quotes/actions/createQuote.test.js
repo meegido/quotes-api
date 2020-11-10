2f65c5df82ae94c2c746c3379e23613d54112c06
Sergio Revilla <replymusic@gmail.com>
2020-11-10 17:25:47 +0100

    Add beforeEach

--- spec/quotes/actions/createQuote.test.js
+++ spec/quotes/actions/createQuote.test.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const createQuote = require("../../../domain/quotes/actions/createQuote");
 const QuotesMongoRepo = require("../../../domain/quotes/infrastructure/quotesMongoRepo");
+const QuoteModel = require("../../../domain/quotes/infrastructure/models");
 
 describe('Create quote', () => {
+    beforeAll(async () => {
+        await mongoose.connect(global.__MONGO_URI__, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+        });
+    });
+
+    beforeEach(async () => {
+        await QuoteModel.deleteMany({})
+    })
+
     it('creates a quote ', async () => {
         const repo = new QuotesMongoRepo()
         const actionResult = await createQuote({sentence: 'This is a sentence'}, repo)
